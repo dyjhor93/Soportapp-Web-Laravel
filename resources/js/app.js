f0e8f4480eaef6388ee5c770c7f27b99e68ab7da
Jhordy Barrera <jbarreraballestas@correo.unicordoba.edu.co>
2023-07-06 17:58:33 -0500

    Solve security problems and update packages to latest versions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,19 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import './bootstrap';
 
-require('./bootstrap');
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+window.Alpine = Alpine;
 
-window.Vue = require('vue');
+Alpine.plugin(focus);
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Alpine.start();
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import $ from "jquery";
+window.$ = $
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Import our custom CSS
+import '../scss/styles.scss'
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
